239e239641f39b597a34105d6a1a53855e710ec5
Gaurav parmar <54779403+thegauravparmar@users.noreply.github.com>
2020-03-08 19:33:53 +0530

    test

--- pages/_app.js
+++ pages/_app.js
@@ -78,7 +78,7 @@ export default class MyApp extends App {
     return (
       <React.Fragment>
         <Head>
-          <title>NextJS Material Kit by Creative Tim</title>
+          <title>Indian Institute of Information Technology, Kota</title>
         </Head>
         <Component {...pageProps} />
       </React.Fragment>
